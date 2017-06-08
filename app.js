19edc995ea1222e441007f477aaddeb5b430f307
myzhoulang <604389771@qq.com>
2017-06-08 23:05:56 +0800

    修改媒体查询，添加bg 样式

--- app.js
+++ app.js
@@ -40,14 +40,12 @@ if (isDev) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-
 
 app.use('/', routes);
 app.use('/components', components);
@@ -64,7 +62,7 @@ app.use('/components', components);
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -75,13 +73,12 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
